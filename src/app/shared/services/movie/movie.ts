565530dd515cdca7f219b70858f2e142160ffbc4
jeevanreddyg <jeevanreddygaddam@gmail.com>
2016-11-27 15:17:34 +0530

    Revert "Ts=es"
    This reverts commit 898d36a892af9a8782d9d296c7cd978bd2a41970.

--- src/app/shared/services/movie/movie.ts
+++
@@ -1,8 +0,0 @@
-export class Movie {
-    id: number;
-    original_title: string = '';
-    overview: string = '';
-    title: string = '';
-    tagline: string = '';
-    poster_path: string = '';
-}
